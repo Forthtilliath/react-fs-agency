47c8fd8d9281079019fd593e2a465b7f78c6a57b
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-07-04 18:03:11 +0200

    ajout delai scroll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Route, Switch, Redirect, useLocation, useHistory } from 'react-router-dom';
 import Home from './pages/Home';
 import Projets from './pages/Projets';
 import Contact from './pages/Contact';
 
 const App = () => {
+    const [lastScroll, setLastScroll] = useState(0);
     const nbProjets = 4;
     const location = useLocation();
     const history = useHistory();
 
     // const arrRoutes = ['', 'projet-1', 'projet-2', 'projet-3', 'projet-4', 'contact'];
 
-    // const smoothScrool = (fn) => {
-    //     setTimeout(fn, 500);
-    // }
+    const wait = useCallback(
+        (fn) => {
+            const time = +new Date();
+            if (time > lastScroll + 500) {
+                fn();
+                setLastScroll(time);
+            }
+        },
+        [lastScroll],
+    );
 
-    const wheelRouter = useCallback((wheel, before, after) => {
-        // Appelé après un event sur le wheel, donc on considère que
-        // wheel ne peut etre égal à 0
-        wheel < 0 ? history.push(after) : history.push(before);
-    },[history]);
+    const wheelRouter = useCallback(
+        (wheel, before, after) => {
+            // Appelé après un event sur le wheel, donc on considère que
+            // wheel ne peut etre égal à 0
+            wait(() => (wheel < 0 ? history.push(after) : history.push(before)));
+        },
+        [history, wait],
+    );
 
     const handleScrollToElement = useCallback(
         (e) => {
@@ -27,7 +38,7 @@ const App = () => {
 
             switch (window.location.href.toString()) {
                 case url:
-                    if (e.wheelDeltaY < 0) history.push('projet-1');
+                    if (e.wheelDeltaY < 0) wait(() => history.push('projet-1'));
                     break;
                 case url + 'projet-1':
                     wheelRouter(e.wheelDeltaY, '', 'projet-2');
@@ -42,12 +53,12 @@ const App = () => {
                     wheelRouter(e.wheelDeltaY, 'projet-3', 'contact');
                     break;
                 case url + 'contact':
-                    if (e.wheelDeltaY > 0) history.push('projet-4');
+                    if (e.wheelDeltaY > 0) wait(() => history.push('projet-4'));
                     break;
                 default:
             }
         },
-        [history, wheelRouter],
+        [history, wait, wheelRouter],
     );
 
     useEffect(() => {
